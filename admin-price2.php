e7dd4993686cee45587733c9f846fab0aca91186
Malik Alrasyid <malikalrasyidbasori.1@gmail.com>
2022-01-18 18:02:04 +0700

    modal

--- admin-price2.php
+++ admin-price2.php
@@ -64,7 +64,7 @@ include('config.php');
                         <td>Art Paper</td>
                         <td>21 x 30 cm</td>
                         <td>Rp. 5.000 / Pcs</td>
-                        <td><button class="btn btn-primary" id="submit" data-bs-toggle="modal" data-bs-target="#modalbeli">Beli</button></td>
+                        <td><button class="btn btn-primary" id="submit" data-bs-toggle="modal" data-bs-target="#modaledit">Edit</button></td>
                     </tr>
 
                     <tr>
@@ -72,7 +72,7 @@ include('config.php');
                         <td>Art Paper</td>
                         <td>22 x 22 cm</td>
                         <td>Rp. 7.000 / Pcs</td>
-                        <td><button class="btn btn-primary" id="submit" data-bs-toggle="modal" data-bs-target="#modalbeli">Beli</button></td>
+                        <td><button class="btn btn-primary" id="submit" data-bs-toggle="modal" data-bs-target="#modaledit">Edit</button></td>
                     </tr>
 
                     <tr>
@@ -80,7 +80,7 @@ include('config.php');
                         <td>Samson Craft</td>
                         <td>21 x 30 cm</td>
                         <td>Rp. 8.000 / Pcs</td>
-                        <td><button class="btn btn-primary" id="submit" data-bs-toggle="modal" data-bs-target="#modalbeli">Edit</button></td>
+                        <td><button class="btn btn-primary" id="submit" data-bs-toggle="modal" data-bs-target="#modaledit">Edit</button></td>
                     </tr>
 
                     <tr>
@@ -88,7 +88,7 @@ include('config.php');
                         <td>Samson Craft</td>
                         <td>22 x 22 cm</td>
                         <td>Rp. 10.000 / Pcs</td>
-                        <td><button class="btn btn-primary" id="submit" data-bs-toggle="modal" data-bs-target="#modalbeli">Edit</button></td>
+                        <td><button class="btn btn-primary" id="submit" data-bs-toggle="modal" data-bs-target="#modaledit">Edit</button></td>
                     </tr>
 
                     <tr>
@@ -96,7 +96,7 @@ include('config.php');
                         <td>Art Paper</td>
                         <td>21 x 30 cm</td>
                         <td>Rp. 5000 / Pcs</td>
-                        <td><button class="btn btn-primary" id="submit" data-bs-toggle="modal" data-bs-target="#modalbeli">Edit</button></td>
+                        <td><button class="btn btn-primary" id="submit" data-bs-toggle="modal" data-bs-target="#modaledit">Edit</button></td>
                     </tr>
 
                     <tr>
@@ -104,7 +104,7 @@ include('config.php');
                         <td>Art Carton</td>
                         <td>21 x 30 cm</td>
                         <td>Rp. 12.000 / Pcs</td>
-                        <td><button class="btn btn-primary" id="submit" data-bs-toggle="modal" data-bs-target="#modalbeli">Edit</button></td>
+                        <td><button class="btn btn-primary" id="submit" data-bs-toggle="modal" data-bs-target="#modaledit">Edit</button></td>
                     </tr>
 
                     <tr>
@@ -112,16 +112,16 @@ include('config.php');
                         <td>Art Carton</td>
                         <td>22 x 22 cm</td>
                         <td>Rp. 15.000 / Pcs</td>
-                        <td><button class="btn btn-primary" id="submit" data-bs-toggle="modal" data-bs-target="#modalbeli">Edit</button></td>
+                        <td><button class="btn btn-primary" id="submit" data-bs-toggle="modal" data-bs-target="#modaledit">Edit</button></td>
                     </tr>
                 </tbody>
             </table>
         </div>
-        <div class="modal fade" id="modalbeli" tabindex="-1" aria-labelledby="modalbeli" aria-hidden="true">
+        <div class="modal fade" id="modaledit" tabindex="-1" aria-labelledby="modaledit" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="modalbeli">Birthday Package Form</h5>
+                        <h5 class="modal-title" id="modaledit">Birthday Package Form</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
